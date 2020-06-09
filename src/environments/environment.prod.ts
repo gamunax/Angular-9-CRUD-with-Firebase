0d903a40bd3ef79b5f2e199af8aca9dff03a128a
Jan Pierre <gamunaxx@gmail.com>
2020-06-09 12:02:04 -0500

    add demo link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCvwmb3e-QJfhtpjCpMPOZ7XwIStE2PLCA',
+    authDomain: 'angular9crud-6aa0e.firebaseapp.com',
+    databaseURL: 'https://angular9crud-6aa0e.firebaseio.com',
+    projectId: 'angular9crud-6aa0e',
+    storageBucket: 'angular9crud-6aa0e.appspot.com',
+    messagingSenderId: '539555860876',
+    appId: '1:539555860876:web:58304963f2e5d96c5a86a9',
+    measurementId: 'G-92350F4436'
+  }
 };
