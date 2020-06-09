c864e2fe575d79353a61cb5e2c543b85e7260ca4
Jan Pierre <gamunaxx@gmail.com>
2020-06-08 19:17:18 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +17,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCvwmb3e-QJfhtpjCpMPOZ7XwIStE2PLCA',
+    authDomain: 'angular9crud-6aa0e.firebaseapp.com',
+    databaseURL: 'https://angular9crud-6aa0e.firebaseio.com',
+    projectId: 'angular9crud-6aa0e',
+    storageBucket: 'angular9crud-6aa0e.appspot.com',
+    messagingSenderId: '539555860876',
+    appId: '1:539555860876:web:58304963f2e5d96c5a86a9',
+    measurementId: 'G-92350F4436'
+  }
 };
 
 /*
